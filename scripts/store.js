2f0f10d1966e73e559c207a4f5d23c5623b30da4
Matthew Day <mday1@central1.com>
2018-10-05 12:23:11 -0400

    Merge bigrefactor into master

--- scripts/store.js
+++ scripts/store.js
@@ -1,28 +1,50 @@
 'use strict';
 
 const store = (function(){
+  const addItem = function(item) {
+    this.items.push(item);
+  };
 
-    let bookmarks = [
-        { id: cuid(), name: 'apples', rating: 1, 
-        description: 'Lorem ipsum dolor sit amet, consectetunon pulvinar lorem felis nec erat',
-        expanded: true 
-        },
-        { id: cuid(), name: 'oranges', rating: 4, description: 'lorem epsim', expanded: false },
-        { id: cuid(), name: 'milk', rating: 2, description: 'lorem epsim', expanded: false },
-        { id: cuid(), name: 'bread', rating: 5, description: 'lorem epsim', expanded: false }
-      ]; 
+  const addingItem = false;
 
-      let adding = false;
+  const toggleAddingItem = function(){
+      addingItem = !addItem;
+  }
 
-      let findById = function(id){
-          return bookmarks.find(bookmark => bookmark.id === id);
-      }
+  const findById = function(id) {
+    return this.items.find(item => item.id === id);
+  };
 
-      let toggleAdding = function(){
-          this.adding = !this.adding;
-      }
+  const findAndDelete = function(id) {
+    this.items = this.items.filter(item => item.id !== id);
+  };
 
-    return {
-        bookmarks, adding, findById, toggleAdding
-    }
+  const toggleExpandedFilter = function() {
+    this.hideCheckedItems = !this.hideCheckedItems;
+  };
+
+  const setSearchTerm = function(term) {
+    this.searchTerm = term;
+  };
+
+  const setErrorMessage = function(errorMessage) {
+    this.errorMessage = errorMessage;
+  };
+
+  return {
+    items: [],
+    searchTerm: '',
+    errorMessage: null,
+    addItem,
+    addingItem,
+    findById,
+    // findAndUpdate,
+    findAndDelete,
+    toggleExpandedFilter,
+    setSearchTerm,
+    // setItemIsEditing,
+    setErrorMessage,
+    toggleAddingItem
+  };
+
 }());
